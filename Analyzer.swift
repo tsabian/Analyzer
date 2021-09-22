f86700731c36b78b8352b70e0952c0213f5aca05
Tiago Oliveira <tiagoo@ciandt.com>
2021-09-22 08:45:04 -0300

    Initial commit

---
+++ Analyzer.swift
@@ -0,0 +1,42 @@
+//
+//  Analyzer.swift
+//  Hyaku
+//
+//  Created by Tiago Almeida de Oliveira on 10/05/21.
+//  Copyright © 2021 Cielo. All rights reserved.
+//
+
+import Foundation
+import os
+
+final class Analyzer {
+    
+    static let shared = Analyzer()
+    
+    private init() { }
+    
+    func logEvent(message: StaticString) {
+        if #available(iOS 14.0, *) {
+            os_log(message, log: .eventCycle, type: .info)
+        } else {
+            debugPrint(message)
+        }
+    }
+    
+    func logEvent(format: StaticString, with value: CVarArg) {
+        let bundle =  Bundle.main.bundleIdentifier ?? "bundle vazio"
+        print("bundle identifier: \(bundle)")
+        if #available(iOS 14.0, *) {
+            os_log(format, log: OSLog.eventCycle, value)
+        } else {
+            debugPrint(format)
+        }
+    }
+}
+
+extension OSLog {
+    private static var subsystem = Bundle.main.bundleIdentifier ?? Environment.appName
+    
+    static let eventCycle = OSLog(subsystem: subsystem, category: "EventFlow")
+    static let viewCycle = OSLog(subsystem: subsystem, category: "ViewFlow")
+}
